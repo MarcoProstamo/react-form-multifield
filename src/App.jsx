b4398600935dc7997770dcbda47f6c14e22d3085
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-04 14:12:06 +0100

    Add → Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./assets/components/layout/Header";
 import Main from "./assets/components/layout/Main";
-import Footer from "./assets/components/layout/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer />
     </>
   );
 }
--- src/assets/components/layout/Header.jsx
+++ src/assets/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="text-bg-light py-3">
+    <header className="text-bg-dark border-1 border-bottom py-3">
       <h1 className="text-center">React Blog Form Multifield</h1>
     </header>
   );
